81e1af3f1cb63c4048b23bef385a7adeb882584a
cocenage <89825630221@mail.ru>
2025-06-17 13:02:37 +0300

    meta

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,8 +4,18 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- про мету не забудь -->
-    <title>{{ $title ?? 'Page Title' }}</title>
+    <meta property="og:title" content="Свежие цветы в Волгограде – Flora | Натуральные и долгостоящие букеты">
+    <meta property="og:image" content="{{ asset('images/ogimage.webp') }}">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
+    <meta property="og:logo" content="{{ asset('images/logo.svg') }}" />
+    <meta property="og:url" content="{{ url()->current() }}">
+    <meta property="og:type" content="website">
+    <meta name="author" content="SHKET Тимофей">
+    <meta name="language" content="ru">
+    @stack('meta')
+
+     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
